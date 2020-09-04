ab1eec3bd5833e05828b33d64ab1b134a7e18965
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-04 12:03:05 +0200

    added stop and start recordin actions

--- src/actions/locationActions.js
+++ src/actions/locationActions.js
@@ -1,5 +1,9 @@
-const stopRecording = (dispatch) => () => {};
-const startRecording = (dispatch) => () => {};
+const stopRecording = (dispatch) => () => {
+  dispatch({ type: 'stop_recording' });
+};
+const startRecording = (dispatch) => () => {
+  dispatch({ type: 'start_recording' });
+};
 const addLocation = (dispatch) => (location) => {
   dispatch({ type: 'add_current_location', payload: location });
 };
--- src/reducers/locationReducer.js
+++ src/reducers/locationReducer.js
@@ -2,6 +2,10 @@ export default (state, action) => {
   const { type, payload } = action;
   const prevState = Object.assign({}, state);
   switch (type) {
+    case 'stop_recording':
+      return { ...prevState, recording: false };
+    case 'start_recording':
+      return { ...prevState, recording: true };
     case 'add_current_location':
       return { ...prevState, currentLocation: payload };
     default:
