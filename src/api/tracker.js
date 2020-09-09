9dfb26915f5017924113956e7eecf7a6b00dcdaf
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-09 14:10:05 +0200

    added token to any request

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,3 +1,18 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
-export default axios.create({ baseURL: 'http://657f9d1e4cc8.ngrok.io' });
+const instance = axios.create({ baseURL: 'http://74cd95fb142f.ngrok.io' });
+
+//this takes care of sending the token along any request
+const onFulfilled = async (axiosRequestConfig) => {
+  const token = await AsyncStorage.getItem('token');
+  if (token) axiosRequestConfig.headers.Authorization = `Bearer ${token}`;
+  return axiosRequestConfig;
+};
+
+const onRejected = (err) => Promise.reject(err);
+
+//sets a funcion to be triggered before any request is performed
+instance.interceptors.request.use(onFulfilled, onRejected);
+
+export default instance;
