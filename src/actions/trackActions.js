f8ee3c6fd30f889d0eae72685218252ce3ba3463
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-09 14:55:03 +0200

    added fetch tracks action

--- src/actions/trackActions.js
+++ src/actions/trackActions.js
@@ -4,5 +4,9 @@ const createTrack = (dispatch) => async (name, locations) => {
   await api.post('/tracks', { name, locations });
   console.log(name, locations.length);
 };
-const fetchTracks = (dispatch) => () => {};
+const fetchTracks = (dispatch) => async () => {
+  const response = await api.get('/tracks');
+  const tracks = response.data;
+  dispatch({ type: 'fetch_tracks', payload: tracks });
+};
 export default { createTrack, fetchTracks };
