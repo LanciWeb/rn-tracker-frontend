c9b35e06dfff605a38ea0783a53bab7b4796d852
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-09 14:55:53 +0200

    added track detail page with coords and map

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import MapView, { Polyline } from 'react-native-maps';
+import { Context as TrackContext } from '../context/TrackContext';
 
-const TrackDetailScreen = () => {
+const TrackDetailScreen = ({ navigation }) => {
+  const _id = navigation.getParam('_id');
+  const { state } = React.useContext(TrackContext);
+  const track = state.find((t) => t._id === _id);
+  const initialCoords = track.locations[0].coords;
+
   return (
-    <View>
-      <Text>Track Detail</Text>
-    </View>
+    <>
+      <Text h1>{track.name}</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          ...initialCoords,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        <Polyline coordinates={track.locations.map((l) => l.coords)} />
+      </MapView>
+    </>
   );
 };
 
 export default TrackDetailScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({ map: { height: 300 } });
