40bd7155c73e2978fb028196e595c3ff1c8b2ab8
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-09 14:55:17 +0200

    added fetch track case

--- src/reducers/trackReducer.js
+++ src/reducers/trackReducer.js
@@ -2,6 +2,8 @@ export default (state, action) => {
   const { type, payload } = action;
   const prevState = Object.assign([], state);
   switch (type) {
+    case 'fetch_tracks':
+      return payload;
     default:
       return prevState;
   }
